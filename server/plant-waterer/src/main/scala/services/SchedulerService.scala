da4a7d1f98b0589ea65ffac9872488b5eeeb9888
Shraddha Jain <shraddha@evenfinancial.com>
2020-12-18 15:37:28 -0500

    Update server/plant-waterer/src/main/scala/services/SchedulerService.scala
    Co-authored-by: Aesa Kamar <AesaKamar@users.noreply.github.com>

--- server/plant-waterer/src/main/scala/services/SchedulerService.scala
+++ server/plant-waterer/src/main/scala/services/SchedulerService.scala
@@ -12,7 +12,7 @@ class SchedulerService(plantsService: PlantsService) {
   def getSchedule(pinId: Int, startTime: Instant, duration: Int): Seq[State] = {
     plantsService.getPlants(pinId = Some(pinId)).headOption match {
       case Some(value) =>
-        val croneSchedule = Cron.unsafeParse(value.schedule)
+        val cronSchedule = Cron.unsafeParse(value.schedule)
         val localDateTime = LocalDateTime.ofInstant(startTime, ZoneId.from(ZoneOffset.UTC))
         (0 until duration).map { increment =>
           val nextTime = localDateTime.plus(increment, ChronoUnit.MINUTES)
