a7d5c896cc2d00a593711a3070934e4d1a9669aa
Andy Wong <andy@evenfinancial.com>
2020-12-22 09:08:56 -0500

    proxy settings

--- app/src/index.js
+++ app/src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 import parser from 'cron-parser';
-import cronTime from "cron-time-generator";
+import cronTime from 'cron-time-generator';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-const query = 'http://localhost:9000/schedules?pinId=123&startTime=2020-01-01T00:00:00Z&duration=10';
-const x = fetch(query).then(res => {
-    console.log(res.json())
-    return JSON.stringify(res)
-})
-console.log(x)
+
+const query = '/schedules?pinId=123&startTime=2020-01-01T00:00:00Z&duration=10';
+const x = fetch(query)
+  .then((res) => res.json())
+  .then((res) => console.log(res));
 
 // console.log(cronTime.everyMinute());
 
@@ -27,7 +26,10 @@ console.log(x)
 //   console.log('Error: ' + err.message);
 // }
 
-const sampleCron = cronTime.everyWeekAt(3, 12, 22)
-console.log(sampleCron)
-const parsedCronExpression = parser.parseExpression(sampleCron);
-console.log("Next Interval", parsedCronExpression.next(parsedCronExpression).toString())
+// const sampleCron = cronTime.everyWeekAt(3, 12, 22);
+// console.log(sampleCron);
+// const parsedCronExpression = parser.parseExpression(sampleCron);
+// console.log(
+//   'Next Interval',
+//   parsedCronExpression.next(parsedCronExpression).toString(),
+// );
